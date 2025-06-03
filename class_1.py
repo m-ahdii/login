8672aa76463a09d516e5fcb32ba0256ffce3e3c3
m-ahdii <mmmoradkhani2003@gmail.com>
2025-06-03 08:01:25 +0300

    added example 2

--- class_1.py
+++ class_1.py
@@ -3,3 +3,11 @@ radius = float(input("enter radius: "))
 area = 3.1416 * radius ** 2
 
 print("Area:", area)
+
+
+
+celsius = float(input("Enter Celsius: "))
+
+fahrenheit = (9/5) * celsius + 32
+
+print("Fahrenheit:",fahrenheit)
