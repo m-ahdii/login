5513755597f768261b4a8489ac8b07f040817022
m-ahdii <mmmoradkhani2003@gmail.com>
2025-06-05 08:07:03 +0300

    new class

---
+++ grades.py
@@ -0,0 +1,31 @@
+labs = []
+for i in range(1, 9):
+    score = float(input(f"Enter score for labs {i} (out of 100): "))
+    labs.append(score)
+    
+aver_lab = sum(labs) / len(labs)
+cover_lab = aver_lab * 0.5 
+
+mid_sem = float(input("Enter mid sem score (out of 100): "))
+end_sem = float(input("Enter end sem score (out of 100): "))
+
+aver_exam = (mid_sem + end_sem) / 2
+cover_exam = aver_exam * 0.5
+
+final_grade = cover_lab + cover_exam
+
+def get_letter_grade(final_grade):
+    if final_grade >= 90:
+        return 'A'
+    elif final_grade >= 80:
+        return 'B'
+    elif final_grade >= 70:
+        return 'C'
+    elif final_grade >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+grade = get_letter_grade(final_grade)
+
+print("You got: ", grade)
